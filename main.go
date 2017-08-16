ae45060d19d1cd56de065ddc58153c0096c208cd
Hiromu OCHIAI <otiai10@gmail.com>
2017-08-16 17:54:59 +0900

    Fix

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 
 	"github.com/otiai10/marmoset"
 
@@ -19,15 +18,9 @@ const pkg = "github.com/otiai10/webm2mp4"
 
 func main() {
 
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		panic("Env var `GOPATH` is not set!")
-	}
-	pkgpath := filepath.Join(gopath, "src", pkg)
-
 	logger = log.New(os.Stdout, fmt.Sprintf("[%s] ", config.AppName()), 0)
 
-	marmoset.LoadViews(filepath.Join(pkgpath, "views"))
+	marmoset.LoadViews("./views")
 
 	r := marmoset.NewRouter()
 
@@ -35,7 +28,7 @@ func main() {
 	r.GET("/status", controllers.Status)
 
 	r.GET("/", controllers.Index)
-	r.Static("/assets", filepath.Join(pkgpath, "assets"))
+	r.Static("/assets", "./assets")
 
 	logger.Printf("listening on port %s", config.Port())
 	err := http.ListenAndServe(config.Port(), r)
