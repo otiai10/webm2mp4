f728d137129007870436cbe62a03631ac148533b
Hiromu OCHIAI <otiai10@gmail.com>
2017-08-18 14:26:51 +0900

    Fix compatibility for Heroku and Docker

--- main.go
+++ main.go
@@ -21,11 +21,7 @@ func main() {
 
 	logger = log.New(os.Stdout, fmt.Sprintf("[%s] ", config.AppName()), 0)
 
-	if cwd, err := filepath.Abs(filepath.Dir(os.Args[0])); err != nil {
-		panic(err)
-	} else {
-		marmoset.LoadViews(filepath.Join(cwd, "views"))
-	}
+	marmoset.LoadViews(viewpath())
 
 	r := marmoset.NewRouter()
 
@@ -33,9 +29,28 @@ func main() {
 	r.GET("/status", controllers.Status)
 
 	r.GET("/", controllers.Index)
-	r.Static("/assets", "./assets")
+	r.Static("/assets", assetpath())
 
 	logger.Printf("listening on port %s", config.Port())
 	err := http.ListenAndServe(config.Port(), r)
 	logger.Println(err)
+}
+
+func cwd() string {
+	if gopath := os.Getenv("GOPATH"); gopath != "" {
+		return filepath.Join(gopath, "src", "github.com/otiai10/webm2mp4")
+	}
+	if cwd, err := filepath.Abs(filepath.Dir(os.Args[0])); err != nil {
+		panic(err)
+	} else {
+		return cwd
+	}
+}
+
+func viewpath() string {
+	return filepath.Join(cwd(), "views")
+}
+
+func assetpath() string {
+	return filepath.Join(cwd(), "assets")
 }
